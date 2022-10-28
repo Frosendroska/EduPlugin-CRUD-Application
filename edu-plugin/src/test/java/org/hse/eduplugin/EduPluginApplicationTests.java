ada9d23bd7854bc17269ee2cad3689518ae4787a
Frosendroska <katyabraun0@gmail.com>
2022-10-28 17:19:55 +0200

    test

---
+++ edu-plugin/src/test/java/org/hse/eduplugin/EduPluginApplicationTests.java
@@ -0,0 +1,15 @@
+package org.hse.eduplugin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class EduPluginApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+}
